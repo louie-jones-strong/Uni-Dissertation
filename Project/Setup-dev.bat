423ecdcf6c5bfccbca7201dd1a57b7f24cd124cf
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2023-05-03 20:39:42 +0100

    create dev venv

--- Project/Setup-dev.bat
+++ Project/Setup-dev.bat
@@ -1,6 +1,6 @@
 py -3.10 -m pip install virtualenv
-py -3.10 -m virtualenv .venv
-call .venv\Scripts\activate.bat
+py -3.10 -m virtualenv .venv-dev
+call .venv-dev\Scripts\activate.bat
 py -m pip install --upgrade pip
 pip install -r requirements.txt
-py Scripts/Main.py
+pip install -r requirements_dev.txt
--- Project/Test.bat
+++ Project/Test.bat
@@ -1,3 +1,3 @@
-call .venv\Scripts\activate.bat
+call .venv-dev\Scripts\activate.bat
 cls
 py -m mypy Scripts/Main.py --config-file Tests/mypy.ini --install-types
