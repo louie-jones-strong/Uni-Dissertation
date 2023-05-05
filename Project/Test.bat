4e452f5ae1bb3476b4804ab420cf92ffcaf93a89
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2023-05-05 01:04:35 +0100

    test fix

--- Project/Test.bat
+++ Project/Test.bat
@@ -1,5 +1,5 @@
 call .venv-dev\Scripts\activate.bat
 cls
-py -m mypy Scripts/Main.py --config-file Tests/mypy.ini --install-types
+py -m mypy Scripts/Main.py --config-file config.ini
 unittest-parallel -q
-flake8 Scripts/
+flake8 Scripts/ --config=config.ini
