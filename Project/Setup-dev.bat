9edf13c458c762814d6941e1f9bbc2788bd31379
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2023-05-10 11:13:44 +0100

    finish docker setup

--- Project/Setup-dev.bat
+++
@@ -1,6 +0,0 @@
-py -3.8 -m pip install virtualenv
-py -3.8 -m virtualenv .venv-dev
-call .venv-dev\Scripts\activate.bat
-py -m pip install --upgrade pip
-pip install -r requirements.txt
-pip install -r requirements_dev.txt
