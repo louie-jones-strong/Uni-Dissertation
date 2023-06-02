712ce9016865e45950bc253efcbb2ee541776bc8
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2023-06-02 13:00:37 +0100

    clean up

--- Project/ReverbServer.py
+++ Project/ReverbServer.py
@@ -1,9 +1,4 @@
 import reverb
-import tensorflow as tf
-
-
-OBSERVATION_SPEC = tf.TensorSpec([10, 10], tf.uint8)
-ACTION_SPEC = tf.TensorSpec([2], tf.float32)
 
 
 simple_server = reverb.Server(
@@ -16,19 +11,9 @@ simple_server = reverb.Server(
 			# Sets Rate Limiter to a low number for the examples.
 			# Read the Rate Limiters section for usage info.
 			rate_limiter=reverb.rate_limiters.MinSize(2),
-			# The signature is optional but it is good practice to set it as it
-			# enables data validation and easier dataset construction. Note that
-			# we prefix all shapes with a 3 as the trajectories we'll be writing
-			# consist of 3 timesteps.
-			signature={
-				'actions':
-					tf.TensorSpec([3, *ACTION_SPEC.shape], ACTION_SPEC.dtype),
-				'observations':
-					tf.TensorSpec([3, *OBSERVATION_SPEC.shape],
-								OBSERVATION_SPEC.dtype),
-			},
 		)
 	],
-	# Sets the port to None to make the server pick one automatically.
-	# This can be omitted as it's the default.
-	port=None)
+	port=8000)
+
+while True:
+	pass
