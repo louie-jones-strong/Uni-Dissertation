4dc12dd8c828a9c18081f8d1a625f7ba92809d9b
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2023-08-01 17:45:19 +0100

    more webserver work

--- src/WebServer/views.py
+++ src/WebServer/views.py
@@ -4,10 +4,15 @@ views = Blueprint("views", __name__)
 
 @views.route("/")
 def Home() -> str:
-	return render_template("index.html", name="Louie")
+	return render_template("overview.html", name="Louie")
 
 
 @views.route("/metrics")
 def Metrics() -> str:
 	# redirect to wandb page
 	return redirect("https://wandb.ai/louiej-s/Dissertation", code=302)
+
+
+@views.route("/episodereview")
+def EpisodeReview() -> str:
+	return render_template("episodereview.html", name="Louie")
