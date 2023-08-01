cb0c6f51948c642f0a1247e36094a2b2fe8e8d95
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2023-08-01 17:36:54 +0100

    more styling

--- src/WebServer/views.py
+++ src/WebServer/views.py
@@ -1,7 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
-def home() -> str:
+def Home() -> str:
 	return render_template("index.html", name="Louie")
+
+
+@views.route("/metrics")
+def Metrics() -> str:
+	# redirect to wandb page
+	return redirect("https://wandb.ai/louiej-s/Dissertation", code=302)
