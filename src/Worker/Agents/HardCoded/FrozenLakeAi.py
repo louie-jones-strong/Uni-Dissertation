6f3ab158b1359687347622e8f79c222adf3ac3e8
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2023-08-30 12:13:42 +0100

    update frozen lake ai to be curated took: 8:30 mins to update

--- src/Worker/Agents/HardCoded/FrozenLakeAi.py
+++ src/Worker/Agents/HardCoded/FrozenLakeAi.py
@@ -2,7 +2,7 @@ import src.Worker.Agents.BaseAgent as BaseAgent
 import src.Common.Utils.SharedCoreTypes as SCT
 import src.Worker.Environments.BaseEnv as BaseEnv
 import typing
-
+import numpy as np
 
 
 class HardCodedAi(BaseAgent.BaseAgent):
@@ -10,12 +10,26 @@ class HardCodedAi(BaseAgent.BaseAgent):
 	def __init__(self, envConfig:SCT.Config, isTrainingMode:bool):
 		super().__init__(envConfig, isTrainingMode)
 
-		self.ActionLookup = [
-			2, 1, 1, 0, 2, 1, 0,
-			2, 1, 1, 0, 0, 1, 0,
-			2, 1, 1, 0, 0, 1, 0,
-			2, 2, 1, 0, 0, 0, 0
-		]
+		self.BehaviorsLookups = {
+			"Normal": [
+				2, 1, 1, 0, 2, 1, 0,
+				2, 1, 1, 0, 0, 1, 0,
+				2, 1, 1, 0, 0, 1, 0,
+				2, 2, 1, 0, 0, 0, 0
+			],
+			"Human": [
+				2, 1, 1, 0, 2, 1, 0,
+				2, 1, 1, 0, 0, 1, 0,
+				2, 1, 1, 0, 0, 1, 0,
+				2, 2, 1, 0, 0, 0, 0
+			],
+			"PlayStyle": [
+				1, 0, 0, 0, 2, 1, 0,
+				2, 1, 1, 0, 0, 1, 0,
+				2, 1, 1, 0, 0, 1, 0,
+				2, 2, 1, 0, 0, 0, 0
+			],
+		}
 		return
 
 	def Reset(self) -> None:
@@ -25,6 +39,29 @@ class HardCodedAi(BaseAgent.BaseAgent):
 	def GetAction(self, state:SCT.State, env:BaseEnv.BaseEnv) -> typing.Tuple[SCT.Action, str]:
 		super().GetAction(state, env)
 
-		action = self.ActionLookup[state]
+		playStyleWeights = self.Config["HardcodedConfig"]["PlayStyleWeights"]
 
-		return action, "HardCoded"
+		actions = []
+		actionWeights = []
+
+		for key, value in self.BehaviorsLookups.items():
+
+			actions.append(value[state])
+			actionWeights.append(playStyleWeights[key])
+
+
+		# normalize weights
+		actionWeights = np.array(actionWeights)
+		actionWeights = actionWeights / np.sum(actionWeights)
+
+
+		action = np.random.choice(actions, p=actionWeights)
+
+
+		reason = {
+			"Actions": actions,
+			"ActionWeights": actionWeights,
+			"AgentType": "HardCoded"
+		}
+
+		return action, reason
